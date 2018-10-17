d00eba38581ac0d48a54c4d2247a355e83121713
jurech <jurech@student.ethz.ch>
2018-10-17 18:36:45 +0200

    screen names not different anymore

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [[ $1 =~ ^[1-8]{1}$ ]]
 then
-	if ![screen -list | grep -q csMatch$1] && ![screen -list | grep -q csWMMatch$1]; 
-	then
+	if ! screen -list | grep -q csMatch$1 ; then
 		HOSTNAME=$(hostname)
 		IP=$(hostname -I | awk '{print substr($1,1)   }')
 		PORT=$(( 27020+$1 ))
@@ -10,7 +9,8 @@ then
 		screen -d -m -S csMatch$1 ~/csgoebotserver/srcds_run -game csgo -console -usercon -tickrate 128 +exec pre +map aim_redline -port $PORT +tv_port $TVPORT +ip $IP +hostname PolyLAN-$HOSTNAME$1 +tv_name PolyTV-$HOSTNAME$1 +tv_title PolyTV-$HOSTNAME$1
 		echo Started $HOSTNAME\.$1 on $IP:$PORT/:$TVPORT
 	else
-		echo Screen csMatch$1 already running.echo Screen csMatch$1 or csWM Match$1 already running.
+		echo Screen csMatch$1 is already running.
+	fi
 else
 	echo Use parameter between 1-8
 fi
--- startWM.sh
+++ startWM.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 if [[ $1 =~ ^[1-8]{1}$ ]]
 then
-	if ![screen -list | grep -q csMatch$1] && ![screen -list | grep -q csWMMatch$1]; 
-	then
+	if ! screen -list | grep -q csMatch$1 ; then
 		HOSTNAME=$(hostname)
 		IP=$(hostname -I | awk '{print substr($1,1)   }')
 		PORT=$(( 27020+$1 ))
 		TVPORT=$(( 27120+$1 ))
-		screen -d -m -S csWMMatch$1 ~/csgoebotserver/srcds_run -game csgo -console -usercon -tickrate 128 +exec preWM +map aim_redline -port $PORT +tv_port $TVPORT +ip $IP +hostname PolyLAN-WM$HOSTNAME$1 +tv_name PolyTV-WM$HOSTNAME$1 +tv_title PolyTV-WM$HOSTNAME$1
+		screen -d -m -S csMatch$1 ~/csgoebotserver/srcds_run -game csgo -console -usercon -tickrate 128 +exec preWM +map aim_redline -port $PORT +tv_port $TVPORT +ip $IP +hostname PolyLAN-WM$HOSTNAME$1 +tv_name PolyTV-WM$HOSTNAME$1 +tv_title PolyTV-WM$HOSTNAME$1
 		echo Started $HOSTNAME\.$1 on $IP:$PORT/:$TVPORT
 	else
-		echo Screen csMatch$1 or csWM Match$1 already running.
+		echo Screen csMatch$1 is already running.
 	fi
 else
 	echo Use parameter between 1-8
