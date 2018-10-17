f43d4dfb34c5a7e6c698378bf2eb7ad59b0ec90e
jurech <jurech@student.ethz.ch>
2018-10-17 12:05:01 +0200

    added Wingam settings

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo apt-get -y install steamcmd nfs-common
+sudo add-apt-repository multiverse 
+sudo add-apt-repository universe 
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install lib32gcc1 steamcmd nfs-common
 echo Installed steamcmd and nfs-common
 
 mkdir ~/freenas
@@ -11,7 +16,7 @@ cp -r -v ~/freenas/cleanserver/* ~/csgoebotserver
 echo Copied CSGOclean to server
 
 cp -r -v ~/freenas/csgoebotserver ~/
-echo Copied Files from git to server
+echo Copied config files to server
 
 sudo umount -f -l ~/freenas
 echo Unmounted freenas
@@ -31,11 +36,17 @@ echo Made update.sh executable
 chmod +x ~/csgoebotserver/start.sh
 echo Made start.sh executable
 
+chmod +x ~/csgoebotserver/startWM.sh
+echo Made startWM.sh executable
+
 chmod +x ~/csgoebotserver/backup.sh
 echo Made backup.sh executable
 
-ln -s ~/csgoebotserver/start.sh ~/startEbot.sh
+ln -s ~/csgoebotserver/start.sh ~/startMatch.sh
 echo Created simlink to start.sh
+
+ln -s ~/csgoebotserver/start.sh ~/startMatchWM.sh
+echo Created simlink to startWM.sh
 
 ln -s ~/csgoebotserver/backup.sh ~/backupEbot.sh
 echo Created simlink to backup.sh
--- startWM.sh
+++ startWM.sh
@@ -7,10 +7,11 @@ then
 		IP=$(hostname -I | awk '{print substr($1,1)   }')
 		PORT=$(( 27020+$1 ))
 		TVPORT=$(( 27120+$1 ))
-		screen -d -m -S csMatch$1 ~/csgoebotserver/srcds_run -game csgo -console -usercon -tickrate 128 +exec pre +map aim_redline -port $PORT +tv_port $TVPORT +ip $IP +hostname PolyLAN-$HOSTNAME$1 +tv_name PolyTV-$HOSTNAME$1 +tv_title PolyTV-$HOSTNAME$1
+		screen -d -m -S csWMMatch$1 ~/csgoebotserver/srcds_run -game csgo -console -usercon -tickrate 128 +exec preWM +map aim_redline -port $PORT +tv_port $TVPORT +ip $IP +hostname PolyLAN-WM$HOSTNAME$1 +tv_name PolyTV-WM$HOSTNAME$1 +tv_title PolyTV-WM$HOSTNAME$1
 		echo Started $HOSTNAME\.$1 on $IP:$PORT/:$TVPORT
 	else
-		echo Screen csMatch$1 already running.echo Screen csMatch$1 or csWM Match$1 already running.
+		echo Screen csMatch$1 or csWM Match$1 already running.
+	fi
 else
 	echo Use parameter between 1-8
 fi
